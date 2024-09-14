b1e3a15bf551e022e9c2a31a7f8a99ed113818fe
Flowolotl <isaac.bayudan@gmail.com>
2024-09-14 16:10:17 -0700

    roles

---
+++ src/events/reaction.ts
@@ -0,0 +1,33 @@
+import { RoleMessageId } from "./startup"
+
+export const events = {
+    Reaction: ["messageReactionAdd"],
+}
+
+export async function Reaction(reaction: any, user: any) {
+    if (reaction.message.author.bot) return
+    
+    if (reaction.message.id !== RoleMessageId) {
+        return
+    }
+
+    if (reaction.emoji.name === "🎯") {
+        if (user.member?.roles.cache.has("1284638359066513460")) {
+           return 
+        }
+
+        user.member?.roles.add(user.guild.roles.cache.get("1284638359066513460"))
+    }
+    // if (reaction.emoji.name === "🎯") {
+        // if (user.id === reaction.message.author.id) {
+            // reaction.message.reactions.removeAll()
+            // reaction.message.edit("🎯")
+        // }
+    // }
+    // else if (reaction.emoji.name === "👽") {
+        // if (user.id === reaction.message.author.id) {
+            // reaction.message.reactions.removeAll()
+            // reaction.message.edit("👽")
+        // }
+    // }
+}
