81983489c9ec1f3139fa831cb90a8845ab587c0a
Flowolotl <isaac.bayudan@gmail.com>
2024-09-14 15:22:26 -0700

    roles test

--- src/events/startup.ts
+++ src/events/startup.ts
@@ -1,5 +1,5 @@
 import { GuildTextBasedChannel } from "discord.js"
-import { InfoMessageContent } from "../assets/config.json"
+import { InfoMessageContent, RolesMessageContent } from "../assets/config.json"
 import { HandleCommands } from "../command-handler"
 import { client } from "../index"
 
@@ -27,6 +27,30 @@ export async function InfoMessage() {
             messages.last()?.edit(InfoMessageContent)
         } else {
             InfoChannel.send(InfoMessageContent)
+        }
+    })
+}
+
+export let RoleMessageId = ""
+
+export async function RolesMessage() {
+    const RolesChannelId = "1281826640028106752"
+    const RolesChannel: GuildTextBasedChannel = client.channels.cache.get(
+        RolesChannelId,
+    ) as GuildTextBasedChannel
+
+    RolesChannel.messages.fetch().then((messages) => {
+        let message
+        if (messages.size > 0) {
+            messages.last()?.edit(RolesMessageContent)
+            message = messages.last()
+        } else {
+            RolesChannel.send(RolesMessageContent).then((sent_message) => {
+                message = sent_message
+                sent_message.react(":dart:")
+                sent_message.react(":alien:")
+            }) 
+
         }
     })
 }
