e2563f9f712d7cbe6e130b56208bb59d060737d0
Flowolotl <isaac.bayudan@gmail.com>
2024-09-14 18:36:00 -0700

    bingus mailing list

--- src/events/startup.ts
+++ src/events/startup.ts
@@ -61,7 +61,6 @@ export async function BingusMailingList() {
     const Users = guild.members.cache.filter((member) => (member.roles.cache.has(BingusMailingRole.id)))
 
     for (const user of Users) {
-        // client.users.send(user.id, ":alien:")
-        console.log(user.user)
+        user[1].user.send(":alien:")
     }
 }
