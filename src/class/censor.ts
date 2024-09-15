da0870c474f13227665d1a55ec652d01b48262a9
Flowolotl <isaac.bayudan@gmail.com>
2024-09-14 20:52:23 -0700

    censoring

--- src/class/censor.ts
+++ src/class/censor.ts
@@ -4,7 +4,6 @@ export function Safe(str: string): boolean {
     for (let a of words) {
         let word: string = a
         let index: number = str.search(`/${word}/gm`)
-        console.log(index)
         if (index > -1) {
             console.log(str, word, index)
             return false
