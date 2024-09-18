da766fd4ef70f839bdadae34f2081afcb41f1d89
Flowolotl <flowolotl@proton.me>
2024-09-17 20:17:39 -0700

    stop censor links

--- src/events/censor.ts
+++ src/events/censor.ts
@@ -1,5 +1,6 @@
 import { Message } from "discord.js"
 import { Clean, isSafe } from "../class/censor.ts"
+import { FirstIndex } from "../class/generic.ts"
 
 export const events = {
     CensorMessages: ["messageCreate"],
@@ -8,6 +9,12 @@ export const events = {
 export async function CensorMessages(message: Message) {
     if (message.author.bot) return
     if (isSafe(message.content)) return
+    if (
+        FirstIndex("http", message.content) == 0 &&
+        /^\S+$/g.test(message.content)
+    ) {
+        return
+    }
 
     let channel = message.channel
     let clean = Clean(message.content)
