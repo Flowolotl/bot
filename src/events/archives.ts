5527995ba383a7a26298960587d3cd232b7cacc6
Flowolotl <flowolotl@proton.me>
2024-09-20 20:49:45 -0700

    fix several bypasses

--- src/events/archives.ts
+++ src/events/archives.ts
@@ -27,14 +27,14 @@ export async function MessageUpdate(oldMessage: Message, newMessage: Message) {
                 TrueArchivesChannelId,
             ) as DMChannel
             trueArchives.send({
-                content: `${time(new Date(message.createdTimestamp))} [${message.channel.name}] [edit] **${message.author?.username}:** ${newMessage.content}`,
+                content: `${time(message.createdAt)} [${message.channel.name}] [edit] **${message.author?.username}:** ${newMessage.content}`,
                 files: attachments,
             })
         } else {
             message.delete()
 
             message.channel.send({
-                content: `${time(new Date(message.createdTimestamp))} [${message.channel.name}] **${message.author?.username}:** ${message.content}`,
+                content: `${time(message.createdAt)} [${message.channel.name}] **${message.author?.username}:** ${message.content}`,
                 files: attachments,
             })
         }
